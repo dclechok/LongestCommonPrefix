ee8b1803a680f7f0ebbbf0231b41f98185278e5e
Daniel C Lechok <dclechok@gmail.com>
2021-02-11 18:54:46 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+
+let wordArray = ["friend", "bug", "big", "slob", "sloth"];
+
+
+function longestCommonPrefix(wordArray){
+    console.log(wordArray);
+		for(let c = 0; c <= wordArray.length; c++){
+			
+		}
+}
+
+longestCommonPrefix(wordArray);
