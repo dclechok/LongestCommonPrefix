a278f9bd589c7c0a3acc3a56fcc39ce27fb7927f
Daniel C Lechok <dclechok@gmail.com>
2021-02-11 22:40:21 +0000

    Code executes properly with expected results, but need to clean up code (including the replace method, etc).

--- index.js
+++ index.js
@@ -19,19 +19,23 @@ function longestCommonPrefix(wordArray){
 		let word = 0; //word Index
 		let letter = 0; //char Index
 	
-		while(wordWithPrefix.length > 1){
-			for(let c = 1; c <= wordWithPrefix.length - 1; c++){
+		while(wordWithPrefix.length > 1){ //while the array has more than one word
+			for(let c = 1; c <= wordWithPrefix.length - 1; c++){ //c is the rest of the words we iterate through in the array
 				while(wordWithPrefix[word][letter] === wordWithPrefix[word + c][letter]){
 					buildingPrefix += wordWithPrefix[word][letter]; //add letters that match to prefix var
 					letter += 1; //check next letters in matching prefixes
 				}
+				
 				letter = 0;
 				//now check same word with next word
-				if(buildingPrefix.length > longestPrefix.length){longestPrefix = buildingPrefix;}
+				if(buildingPrefix.length > longestPrefix.length){
+					longestPrefix = buildingPrefix;
+				}
+				buildingPrefix = 0; //reset buildingPrefix
 			}
 			wordWithPrefix = wordWithPrefix.slice(1); //move on from first word
 		}
-		console.log(longestPrefix);
+		console.log(longestPrefix.replace(0, ''));
 }
 
 longestCommonPrefix(wordArray);
